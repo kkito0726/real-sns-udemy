79fcad73062563bdebdf0269560614598b0455f1
kkito0726 <k.kito0726@gmail.com>
2023-01-22 15:30:27 +0900

    MongoDBと接続してユーザー登録機能を実装

--- server.js
+++ server.js
@@ -4,7 +4,21 @@ const userRouter = require("./routes/users");
 const authRouter = require("./routes/auth");
 const postRouter = require("./routes/posts");
 const PORT = 3000;
+const mongoose = require("mongoose");
+require("dotenv").config();
 
+// データベース接続
+mongoose
+  .connect(process.env.MONGOURL)
+  .then(() => {
+    console.log("DBと接続中・・・");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// ミドルウェア
+app.use(express.json());
 app.use("/api/users", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/posts", postRouter);
